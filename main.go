03df75f9fad2ecd493b1b07c8daf86dc2aaf1d91
console8833 <console8833@outlook.com>
2024-02-06 12:38:19 +0800

    code: separate repl function from main

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	fmt.Print("Go-Pokedex> ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	text := scanner.Text()
-
-	fmt.Println("echoing:", text)
+	startRepl()
 }
